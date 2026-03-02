7b515c7cb29b4146a1c33119ca33ebf56f3ae148
[Mohammad Ali Hasan] <[muhdalibd@gmail.com]>
2026-03-02 17:47:22 +0600

    Update

---
+++ Basics/basics.py
@@ -0,0 +1,4 @@
+## Introduction to Programming
+#  Programming == creating a set of instructions - that tells a
+#       computer how to perform a task.
+#       Set of instruction called => Program
