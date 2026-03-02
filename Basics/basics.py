320901645010fb12a933de84ee7b64bf63abfdbb
[Mohammad Ali Hasan] <[muhdalibd@gmail.com]>
2026-03-02 19:55:20 +0600

    Update

--- Basics/basics.py
+++ Basics/basics.py
@@ -1,4 +1,102 @@
-## Introduction to Programming
-#  Programming == creating a set of instructions - that tells a
-#       computer how to perform a task.
-#       Set of instruction called => Program
+### Introduction to Programming
+#   Programming == creating a set of instructions - that tells
+#       a computer how to perform a task.
+#       Those set of instruction called => Program
+
+#   In Python Programming Language:
+#       Source Code => Compiler => Byte Code => Interpreter => Mechine Code
+
+
+
+### Introduction to Python and DSA
+#   One of the most famous language
+#   Easy to learn & understand
+#   Beginer friendly
+#   Concise Code & Open Source
+#   Versatile => Web Dep., AI/ML, Data Science, Data Analytics, IOT
+
+#   Download => VS Code & Python
+
+
+### Starting with Python
+# print("Hello World!")
+
+
+''' Chapter 1: Basics
+    01. Basics Program
+    02. Python CLI
+    03. Comments
+    04. Indentation
+    05. Variables
+    06. Keywords in Python
+    07. Data Types
+    08. ASCII and Unicode
+    09. Taking Input
+    10. Operators
+    11. Typecasting & type() function
+    12. Hierarchy'''
+
+
+#   Basic Program in Python
+# print("Hello World!")
+#   How to move to next line
+# print("Hello,\nWorld!")
+
+
+#   Pyhon CLI
+''' Command Line Interface
+        Command => python3 (for Start)
+        REPL => Read Evaluate Print Loop
+        Command => exit() or ^D '''
+
+
+
+#   Python Comments
+'''This is a multi line comment'''
+
+
+#   Python Indentation
+'''print("Hello World!")
+    print("Hello World") # This is called Indentation Error'''
+#   Indentation => Tab Space Basicaly
+
+
+#   Variables and their Declarations
+'''
+    Variables => container used to store data.
+        In programming container means Memory Space.
+    Data can be different types. (Integer, Float, String, Boolean, NULL)
+    In Python, Variables do not need to be declared with any particular type.
+            Python is dynamically typed.
+'''
+#   Variable Naming Rules:
+'''
+    1. A variable name must start with a letter or underscore character.
+    2. A variable name cannot start with a number.
+    3. A variable name can only contain alphanumeric and underscores (A-z,0-9,_)
+    4. Variable names are case-sensitive (count, Count & COUNT are three different variables)
+    5. A variable name cannot be any of the Python Keywords.
+
+        is_student => Snake Case (Prefered in Python)
+        isStudent => Lower Camel Case
+'''
+#   String
+name = "Isha"
+# print(type(name))   # <class 'str'>
+#   Integer
+roll_no = 17
+# print(type(roll_no))    # <class 'int'>
+#   Float
+percentage = 95.8
+# print(type(percentage)) # <class 'float'>
+#   Boolean
+is_student = True
+# print(type(is_student)) # <class 'bool'>
+
+# print(name, roll_no, percentage, is_student)
+#   Updating the value:
+# percentage = 94
+# print(name, roll_no, percentage, is_student)
+
+#   Keywords in Python:
+#       Search in Internet.
