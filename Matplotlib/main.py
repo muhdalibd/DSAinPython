43f18c05c4529ca76c760b5205e5ff7026714b66
[Mohammad Ali Hasan] <[muhdalibd@gmail.com]>
2026-08-06 09:53:51 +0600

    Update

--- Matplotlib/main.py
+++ Matplotlib/main.py
@@ -1,8 +1,5 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-# fig, ax = plt.subplots(2, 1)
-# plt.show()
-
-img = np.asarray(Image.open("C:\Users\hp\Pictures\meigen-2074347894455607536-2.jpg"))
-print(repr(img))
+fig, ax = plt.subplots(2, 1)
+plt.show()
